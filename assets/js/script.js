fd6860d995cddaf8331c6a0bf35f1b294029d430
graham <gr4ham18@gmail.com>
2021-11-22 19:27:23 -0500

    adding extra hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,14 @@ $(document).ready(function () {
             }
         })
     }
+    $("#hour0 .description").val(localStorage.getItem("hour0"));
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
+    $("#hour6 .description").val(localStorage.getItem("hour6"));
+    $("#hour7 .description").val(localStorage.getItem("hour7"));
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -38,6 +46,13 @@ $(document).ready(function () {
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+    $("#hour19 .description").val(localStorage.getItem("hour19"));
+    $("#hour20 .description").val(localStorage.getItem("hour20"));
+    $("#hour21 .description").val(localStorage.getItem("hour21"));
+    $("#hour22 .description").val(localStorage.getItem("hour22"));
+    $("#hour23 .description").val(localStorage.getItem("hour23"));
+    
 
     timeTracker();
 })
